a1ac4f3f92f922304f98dd62661c5eb0e259e23f
Wayne Tsou <wayne60406@gmail.com>
2025-05-24 22:02:09 +0800

    feat: modify theme

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,27 +1,18 @@
-import rss from '@astrojs/rss';
-import {site} from "../consts";
-import getUrl from "../utils/getUrl.js";
-import {getCollection} from "astro:content";
-import {getCollectionByName} from "@/utils/getCollectionByName";
-import {sortPostsByDate} from "@/utils/sortPostsByDate";
+import rss from "@astrojs/rss";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
+import { getCollection } from "astro:content";
 
-export async function GET(context) {
-  // const blogs = await getCollection('blog')
-  const blogs = await getCollectionByName('blog')
-  let sortPosts = await sortPostsByDate(blogs);
-  let blog = sortPosts.splice(0, 20);
-
+export async function get(context) {
+  const blog = await getCollection("blog");
   return rss({
-    title: site.title,
-    description: site.description,
-    site: site.url,
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: import.meta.env.SITE,
     items: blog.map((post) => ({
       title: post.data.title,
-      pubDate: post.data.date,
-      description: post.data.description? post.data.description : post.body.substring(0, 140).replace(/#/gi, "") + "...",
-      // Compute RSS link from post `slug`
-      // This example assumes all posts are rendered as `/blog/[slug]` routes
-      link: `${getUrl("/blog/")}${post.slug}/`,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
     })),
   });
 }
